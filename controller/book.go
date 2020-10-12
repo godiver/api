6385c8bcd2f39337a8120622a91d449da88d2173
Kyohei Seo <kyo199333@gmail.com>
2020-10-12 19:18:58 +0900

    add request handler to search books from rakuten

--- controller/book.go
+++ controller/book.go
@@ -35,3 +35,27 @@ func Books(c echo.Context) error {
 type Parameter struct {
 	Title string `json:"title" form:"title" query:"title"`
 }
+
+func SearchBooks(c echo.Context) error {
+	id := env.Env.API.Rakuten.ID
+	parameter := new(Parameter)
+	if err := c.Bind(parameter); err != nil {
+		return c.String(http.StatusBadRequest, "時間をおいて再度実行してください。")
+	}
+
+	url := fmt.Sprintf("https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?applicationId=%s&sort=sales&title=%s", id, parameter.Title)
+	res, err := http.Get(url)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "時間をおいて再度実行してください。")
+	}
+	defer res.Body.Close()
+	var r io.Reader = res.Body
+	r = io.TeeReader(r, os.Stderr)
+	var books interface{}
+	err = json.NewDecoder(r).Decode(&books)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c.JSON(http.StatusOK, books)
+}
