aede26640aefa5dcfbb17554c5135d78b9f32aca
Nnsuke24 <nnsuke24@gmail.com>
2021-03-26 20:36:07 +0900

    add 特定のビデオ情報を返すメソッド（API）作成

--- controller/video.go
+++ controller/video.go
@@ -57,6 +57,52 @@ func GetVideos(c echo.Context) error {
 	return c.JSON(http.StatusOK, response)
 }
 
+func GetVideo(c echo.Context) error {
+	videoId := c.Param("videoId")
+	developerKey := env.Env.API.Youtube.Key
+
+	client := &http.Client{
+		Transport: &transport.APIKey{Key: developerKey},
+	}
+
+	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Error creating new YouTube client: %v", err)
+	}
+
+	arr := []string{"id", "snippet"}
+
+	// Make the API call to YouTube.
+	call := service.Search.List(arr).
+		Q(videoId).
+		MaxResults(1)
+	response, err := call.Do()
+	// handleError(err, "")
+
+	// Group video, channel, and playlist results in separate lists.
+	videos := make(map[string]string)
+	channels := make(map[string]string)
+	playlists := make(map[string]string)
+
+	// Iterate through each item and add it to the correct list.
+	for _, item := range response.Items {
+		switch item.Id.Kind {
+		case "youtube#video":
+			videos[item.Id.VideoId] = item.Snippet.Title
+		case "youtube#channel":
+			channels[item.Id.ChannelId] = item.Snippet.Title
+		case "youtube#playlist":
+			playlists[item.Id.PlaylistId] = item.Snippet.Title
+		}
+	}
+
+	printIDs("Videos", videos)
+	printIDs("Channels", channels)
+	printIDs("Playlists", playlists)
+
+	return c.JSON(http.StatusOK, response)
+}
+
 // Print the ID and title of each result in a list as well as a name that
 // identifies the list. For example, print the word section name "Videos"
 // above a list of video search results, followed by the video ID and title
